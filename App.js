b9c559fb2d09c1773ecef4f7f364a73aa649aef7
AsynChrnousCalls <wc.hussein000@gmail.com>
2023-03-10 16:05:39 +0500

    Initial Commit After setup

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello UberWorld</Text>
+      <Text>asdfdf</Text>
       <StatusBar style="auto" />
     </View>
   );
